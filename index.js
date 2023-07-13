a9faa7eef7e4a0a712d126a680578ec653465f38
raghavSatyam0069 <134059721+raghavSatyam0069@users.noreply.github.com>
2023-07-13 20:25:54 +0530

    1 file modified

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 // run `node index.js` in the terminal
-console.log('hello');
+for (let i = 1; i <= 100; i++) {
+  i % 3 === 0 && i % 5 === 0
+    ? console.log('amazon')
+    : i % 3 === 0
+    ? console.log('google')
+    : i % 5 === 0
+    ? console.log('facebook')
+    : console.log(i);
+}
